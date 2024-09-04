1855922ff923e5e9b185e022de1932012562e80d
CTEthanMoss <ctethanmoss@gmail.com>
2024-09-04 14:24:03 -0400

    Add files via upload

---
+++ JavaScriptIntro/scripts/script.js
@@ -0,0 +1,14 @@
+/* 
+    Student Name: Ethan Moss
+    File Name: script.js
+    Date: 1/3/24
+*/
+
+// Function to display content
+function content() {
+    var text = document.getElementById("new");
+    text.textContent = "Hello. I am a new paragraph that was created by using javascript!";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+  }
+  
